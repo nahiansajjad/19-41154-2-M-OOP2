49b7851cdb778153309baf3a731742ffc164bfae
Nahian Sajjad <sajjadrocks97@gmail.com>
2021-02-08 09:25:23 +0600

    code uploded

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+
+            Console.Write("Even Numebrs: ");
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i == n)
+                    {
+                        Console.Write(i);
+                    }
+                    else
+                    {
+                        Console.Write(i + ", ");
+                    }
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
